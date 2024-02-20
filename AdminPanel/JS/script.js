935273fd302d5ae39505be40f4204c6e19e8e51c
Meet Asodariya <asodariyameet16@gmail.com>
2024-02-20 18:02:20 +0530

    Added Admin Pannel

---
+++ AdminPanel/JS/script.js
@@ -0,0 +1,14 @@
+// Navbar Toggle BUtton Javascript
+
+
+const menu = document.querySelector('.menu-1');
+const navbar  = document.querySelector('.navbar-main-container')
+
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('change')
+    navbar.classList.toggle('change')
+})
+
+//End of Navbar Toggle BUtton Javascript
+
+ 
